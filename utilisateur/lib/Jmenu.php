1e12c7d49978b49f96bb0a57516125edd2b22bc9
greeg4 <mathieu405@hotmail.com>
2016-01-10 14:06:37 +0100

    Utilisateur
    Jliste_include, Jmenu, autoload

---
+++ utilisateur/lib/Jmenu.php
@@ -0,0 +1,7 @@
+<ul id="menu">
+ <li class="bottomLine"><a href="index.php?page=accueil">Accueil</a></li>
+  <li class="bottomLine"><a href="index.php?page=creercompte">Création de compte</a></li>
+ <li class="bottomLine"><a href="index.php?page=catalogue">Catalogue et commande</a></li>
+ <li class="bottomLine"><a href="index.php?page=rechercherjeu">Recherche</a></li>
+ <li class="bottomLine"><a href="index.php?page=contact"> Contact </a></li>
+</ul>
